097eee393c0a65cd4163a734a5a550ab41d66a24
Soliprem <franci.solidoro@gmail.com>
2024-08-20 17:36:01 +0200

    update

--- nvim/plugin/eyeliner.lua
+++ nvim/plugin/eyeliner.lua
@@ -5,6 +5,6 @@
 
 -- Highlights unique characters for f/F and t/T motions
 require('eyeliner').setup {
-  -- highlight_on_key = true, -- show highlights only after key press
-  dim = true, -- dim all other characters
+  highlight_on_key = true, -- show highlights only after key press
+  -- dim = true, -- dim all other characters
 }
