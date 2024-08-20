a3d15a243bb7a1c5a0a26274599d40db3a883846
Soliprem <franci.solidoro@gmail.com>
2024-08-21 01:07:33 +0200

    update

--- nvim/ftplugin/javascript.lua
+++ nvim/ftplugin/javascript.lua
@@ -1,5 +1,5 @@
 -- Exit if the language server isn't available
-if vim.fn.executable('typescript-language-server') ~= 1 then
+if vim.fn.executable('typescript-language-server --stdio') ~= 1 then
   return
 end
 
@@ -11,7 +11,7 @@ local root_files = {
 
 vim.lsp.start {
   name = 'typescript-language-server',
-  cmd = { 'typescript-language-server' },
+  cmd = { 'typescript-language-server --stdio' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
--- nvim/ftplugin/typescript.lua
+++ nvim/ftplugin/typescript.lua
@@ -1,5 +1,5 @@
 -- Exit if the language server isn't available
-if vim.fn.executable('typescript-language-server') ~= 1 then
+if vim.fn.executable('typescript-language-server --stdio') ~= 1 then
   return
 end
 
@@ -10,7 +10,7 @@ local root_files = {
 }
 
 vim.lsp.start {
-  name = 'typescript-language-server',
+  name = 'typescript-language-server --stdio',
   cmd = { 'typescript-language-server' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
